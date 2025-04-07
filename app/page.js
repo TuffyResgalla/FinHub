0bfa0c5d9c39ab34d90323d26de8c54807512870
TuffyResgalla <45566845+TuffyResgalla@users.noreply.github.com>
2025-04-07 11:17:13 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 'use client';
 
 import React, { useState } from "react";
+import '../globals.css';
 import { Input } from "../components/ui/input";
 import { Button } from "../components/ui/button";
 import { Card, CardContent } from "../components/ui/card";
